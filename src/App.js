1a0d7c2f859a9d424f5f2c510b97ffd5a1b58de3
Brent <jbmsharpe@gmail.com>
2023-03-02 12:31:05 -0500

    added delete button for task

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import { useState } from 'react';
 
 function App() {
+  const [tasks, setTasks] = useState(
+    [
+        {
+            id: 1,
+            text: 'Buy Groceries',
+            day: 'March 1, 2023 at 12:27',
+            reminder: true
+        },
+        {
+            id: 2,
+            text: 'Clean Kitchen',
+            day: 'March 1, 2023 at 12:27',
+            reminder: false
+        },
+        {
+            id: 3,
+            text: 'Buy rustoleum for car',
+            day: 'March 1, 2023 at 12:27',
+            reminder: true
+        }
+    ])
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,29 +1,11 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Buy Groceries',
-        day: 'March 1, 2023 at 12:27',
-        reminder: true
-    },
-    {
-        id: 2,
-        text: 'Clean Kitchen',
-        day: 'March 1, 2023 at 12:27',
-        reminder: false
-    },
-    {
-        id: 3,
-        text: 'Buy rustoleum for car',
-        day: 'March 1, 2023 at 12:27',
-        reminder: true
-    }
-]
+import Task from './Task'
 
-function Tasks() {
+function Tasks({tasks, onDelete}) {
+
   return (
     <div>
         {tasks.map((task) => (
-            <h3>{task.text}</h3>
+            <Task key={task.id} task={task} onDelete={onDelete}/>
         ))}
     </div>
   )
