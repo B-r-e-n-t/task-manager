fab1eb7e6c1588345fd4f780900bf089ce4a3094
Brent <jbmsharpe@gmail.com>
2023-03-03 10:56:33 -0500

    created form to add tasks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask';
 import { useState } from 'react';
 
 function App() {
@@ -30,10 +31,16 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask}/>
+      <AddTask />
+      { tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle= {toggleReminder}/> : ('No tasks to show')}
     </div>
   );
 }
